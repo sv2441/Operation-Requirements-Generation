7a5807a74246828cc2f35578ac5e68f49f4fab5a
Sandeep Vishwakarma <62471058+sv2441@users.noreply.github.com>
2023-11-22 00:31:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+## import the CSV file and output the CSV file 
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Operation Requirements Generation",
+    page_icon="👋",
+)
+
+st.image('logo.png')
+st.title("👨‍💻 Operation Requirements Generation")
